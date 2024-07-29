a24c3623f416dee663c2d8fe27d48036a820f6f7
pankajtwts <pankajshadev@gmail.com>
2024-07-29 22:10:53 +0530

    feat: added zod schema for validation

--- apps/create_problem/src/types.ts
+++ apps/create_problem/src/types.ts
@@ -1,3 +1,5 @@
+import z from "zod";
+
 export class ApiError extends Error {
     statusCode: number;
     details?: unknown;
@@ -8,3 +10,19 @@ export class ApiError extends Error {
         this.details = details;
     }
 }
+
+export const CreateProblem = z.object({
+    title: z.string().min(3, "Must be 3 characters long."),
+    description: z.string().min(10, "Must be 10 characters long."),
+    difficulty: z.string().toLowerCase().refine(value=> ["easy", "medium", "hard"].includes(value), "Must be any one of these [easy, hard, medium]"),
+    editorial: z.string().optional(),
+    testCases: z.array(z.object({
+        input: z.string(),
+        output: z.string(),
+    })),
+}).strict();
+
+export type CreateProblem = z.infer<typeof CreateProblem>;
+
+export const UpdateProblem = CreateProblem.partial();
+export type UpdateProblem = z.infer<typeof UpdateProblem>;
