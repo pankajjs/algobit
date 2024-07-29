5fb6f2ecf88cc8a31af4ea748a0fa882fe20513d
pankajtwts <pankajshadev@gmail.com>
2024-07-29 22:21:58 +0530

    feat: added v1 patch route

--- apps/create_problem/src/routes/v1/problem.routes.ts
+++ apps/create_problem/src/routes/v1/problem.routes.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response, Router } from "express";
 import StatusCodes from "http-status-codes";
 import { db } from "../../db";
-import { ApiError, CreateProblem } from "../../types";
+import { ApiError, CreateProblem, UpdateProblem } from "../../types";
 import { sanitizedMarkdown } from "../../utils";
 
 const problemRouter = Router();
@@ -107,6 +107,49 @@ problemRouter.post("/", async (req: Request, res: Response, next: NextFunction) 
         return res.status(StatusCodes.CREATED).json({
             success: true, 
             message: "Successfully created new problem",
+            data: problem,
+            error: {}
+        })
+    }catch(error: any){
+        next(error);
+    }
+})
+
+problemRouter.patch("/:id", async (req: Request, res: Response, next: NextFunction) => {
+    try{
+        const id = req.params.id;
+        const parsedSchema = await UpdateProblem.safeParseAsync(req.body);
+
+        if(!parsedSchema.success){
+            throw new ApiError("Invalid request body", StatusCodes.BAD_REQUEST, 
+                parsedSchema.error.issues.map(issue=>({ messgae: issue.message, path: issue.path}))
+            );
+        }
+
+        const existingProblem = await db.problem.findFirst({
+            where: {
+                id: id,
+            }
+        });
+
+        if(!existingProblem){
+            throw new ApiError(`Problem not found with id=${id}`, StatusCodes.NOT_FOUND);
+        }
+
+        if(parsedSchema.data.description){
+            parsedSchema.data.description = sanitizedMarkdown(parsedSchema.data.description);
+        }
+
+        const problem = await db.problem.update({
+            where: {
+                id: existingProblem.id,
+            },
+            data: parsedSchema.data,
+        });
+
+        return res.status(StatusCodes.OK).json({
+            success: true,
+            message: "Successfully updated a problem",
             data: problem,
             error: {}
         })
