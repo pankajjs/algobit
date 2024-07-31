3e780944c84001517f2277ea418c4fa9061ceedc
pankajtwts <pankajshadev@gmail.com>
2024-07-31 18:16:32 +0530

    chore: refactor

--- apps/executor/src/api/fetchproblem.ts
+++ apps/executor/src/api/fetchproblem.ts
@@ -4,7 +4,7 @@ import { TestCases } from "../helper/types";
 
 const fetchProblemTestCase = (problemId: string) => {
     return new Promise<TestCases>((resolve, reject)=>{
-        axios.get(`${ServerConfig.Problem_Admin_Service}/api/v1/problems/${problemId}`)
+        axios.get(`${ServerConfig.ADMIN_SERVICE_URI}/api/v1/problems/${problemId}`)
         .then(res=>
             resolve(res.data.data.testCases))
         .catch(error=>{
