a922c849548af296e651c3d9e524cd21617bbd90
pankajtwts <pankajshadev@gmail.com>
2024-07-31 18:56:23 +0530

    feat: added constants

--- apps/executor/src/helper/constants.ts
+++ apps/executor/src/helper/constants.ts
@@ -10,3 +10,6 @@ export const JAVA_IMAGE = "openjdk:8-jdk-slim";
 
 export const SUBMISSION_REQUEST_QUEUE = "SubmissionRequestQueue";
 export const SUBMISSION_REQUEST_JOB= "SubmissionRequestJob";
+
+export const SUBMISSION_RESPONSE_QUEUE = "SubmissionResponseQueue";
+export const SUBMISSION_RESPONSE_JOB = "SubmissionResponseJob";
