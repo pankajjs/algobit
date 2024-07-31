d2d785fb5bb0c0597e003f443a4c63291bc09355
pankajtwts <pankajshadev@gmail.com>
2024-07-31 18:59:02 +0530

    fix: publish submission response in queue

--- apps/executor/src/worker/submission_worker.ts
+++ apps/executor/src/worker/submission_worker.ts
@@ -1,15 +1,27 @@
 import { Job, Worker } from "bullmq";
 import { redisConnection } from "../helper/redis";
-import { JobPayload } from "../helper/types";
 import { codeExecutorFactory } from "../code_executor/factory";
-import { SUBMISSION_REQUEST_QUEUE } from "../helper/constants";
+import { SUBMISSION_REQUEST_QUEUE, SUBMISSION_RESPONSE_JOB } from "../helper/constants";
+import { publishJob } from "../helper/publisher";
+import { submissionResponseQueue } from "../queue/submission_response";
+import { RequestJobPayload } from "../helper/types";
 
 const submissionWorker = new Worker(
         SUBMISSION_REQUEST_QUEUE,
-        async (job: Job<JobPayload> ) => {
+        async (job: Job<RequestJobPayload> ) => {
             const executor = codeExecutorFactory(job.data.language);
             const response = await executor.execute(job.data)
+            
             console.log(response);
+            
+            await publishJob({
+                name: SUBMISSION_RESPONSE_JOB,
+                queue: submissionResponseQueue,
+                payload: {
+                    status: response.status,
+                    submissionId: response.submissionId,
+                }
+            })
         },
         {
             connection: redisConnection,
