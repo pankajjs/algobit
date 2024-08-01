7008adbed44d45d547b50a98edf674dbd6877735
pankajtwts <pankajshadev@gmail.com>
2024-08-01 23:59:21 +0530

    chore: refactor worker job

--- apps/executor/src/worker/submission_worker.ts
+++ apps/executor/src/worker/submission_worker.ts
@@ -1,27 +1,18 @@
 import { Job, Worker } from "bullmq";
 import { redisConnection } from "../helper/redis";
-import { codeExecutorFactory } from "../code_executor/factory";
-import { SUBMISSION_REQUEST_QUEUE, SUBMISSION_RESPONSE_JOB } from "../helper/constants";
-import { publishJob } from "../helper/publisher";
-import { submissionResponseQueue } from "../queue/submission_response";
+import { SUBMISSION_REQUEST_QUEUE } from "../helper/constants";
 import { RequestJobPayload } from "../helper/types";
+import { submissionRequestJobHandler } from "../helper/job_handler";
 
 const submissionWorker = new Worker(
         SUBMISSION_REQUEST_QUEUE,
         async (job: Job<RequestJobPayload> ) => {
-            const executor = codeExecutorFactory(job.data.language);
-            const response = await executor.execute(job.data)
-            
-            console.log(response);
-            
-            await publishJob({
-                name: SUBMISSION_RESPONSE_JOB,
-                queue: submissionResponseQueue,
-                payload: {
-                    status: response.status,
-                    submissionId: response.submissionId,
-                }
-            })
+            try{
+                await submissionRequestJobHandler(job.data);
+            }catch(error){
+                console.log(error);
+                throw error;
+            }
         },
         {
             connection: redisConnection,
