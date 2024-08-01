bbd7a51ea7478f5f9cbb8e4dd6d35627ed14cf2f
pankajtwts <pankajshadev@gmail.com>
2024-08-01 23:43:39 +0530

    fix: return problem details

--- apps/executor/src/api/fetchproblem.ts
+++ apps/executor/src/api/fetchproblem.ts
@@ -1,12 +1,12 @@
 import axios from "axios";
 import { ServerConfig } from "../config/server_config";
-import { TestCases } from "../helper/types";
+import { Problem } from "../helper/types";
 
-const fetchProblemTestCase = (problemId: string) => {
-    return new Promise<TestCases>((resolve, reject)=>{
+const fetchProblemDetails = (problemId: string) => {
+    return new Promise<Problem>((resolve, reject)=>{
         axios.get(`${ServerConfig.ADMIN_SERVICE_URI}/api/v1/problems/${problemId}`)
         .then(res=>
-            resolve(res.data.data.testCases))
+            resolve(res.data.data))
         .catch(error=>{
             reject(error.message);
         })
@@ -14,5 +14,5 @@ const fetchProblemTestCase = (problemId: string) => {
 }
 
 export {
-    fetchProblemTestCase,
+    fetchProblemDetails,
 }
