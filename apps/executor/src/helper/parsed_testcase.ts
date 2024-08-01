22628034b2bbd4c4353ac0892f2eca03560c81b8
pankajtwts <pankajshadev@gmail.com>
2024-08-01 23:57:51 +0530

    fix: trim testcases input and output

--- apps/executor/src/helper/parsed_testcase.ts
+++ apps/executor/src/helper/parsed_testcase.ts
@@ -4,9 +4,9 @@ export const parsedTestCases = (testCases: TestCases): {
     input: string,
     output: string,
 } => {
-    const input = testCases.map(tc=>tc.input).join("\n") || "";
-    const output = testCases.map(tc=>tc.output).join("\n") + "\n" || "";
-
+    const input = testCases.map(tc=>tc.input).join("\n").trim() || "";
+    const output = testCases.map(tc=>tc.output).join("\n").trim() || "";
+    
     return {
         input, output
     }
