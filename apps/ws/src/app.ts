52f153c350a14764f5bbca55db3761ab8e496f35
pankajtwts <pankajshadev@gmail.com>
2024-08-01 13:17:23 +0530

    feat: added middlewares

--- apps/ws/src/app.ts
+++ apps/ws/src/app.ts
@@ -1,12 +1,21 @@
 import express from "express";
 import { createServer } from "http";
+import StatusCodes from "http-status-codes";
+import { errorHandler } from "./helper/middleware";
 
 const app = express();
+
+app.use(express.json())
+app.use(express.text());
+app.use(express.urlencoded({extended: true}));
+
 const server = createServer(app);
 
 app.get("/healthcheck", (_, res)=>{
-    return res.status(200).send("Ws service is alive.")
+    return res.status(StatusCodes.OK).send("Ws service is alive.")
 })
+
+app.use(errorHandler);
 
 export {
     server,
