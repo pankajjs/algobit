d0e0bd0d4cc0ef1d6fdac6e8316ccc261a070e42
pankajtwts <pankajshadev@gmail.com>
2024-08-01 13:20:01 +0530

    feat: added socket connection

--- apps/ws/src/app.ts
+++ apps/ws/src/app.ts
@@ -2,6 +2,9 @@ import express from "express";
 import { createServer } from "http";
 import StatusCodes from "http-status-codes";
 import { errorHandler } from "./helper/middleware";
+import { ApiError } from "./helper/types";
+import { redis } from "./helper/redis";
+import { Server } from "socket.io";
 
 const app = express();
 
@@ -13,6 +16,30 @@ const server = createServer(app);
 
 app.get("/healthcheck", (_, res)=>{
     return res.status(StatusCodes.OK).send("Ws service is alive.")
+})
+
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+}});
+
+let user = "1"
+
+io.on("connection", (socket)=>{
+    console.log("user connected", user)
+    redis.set(user, socket.id)
+    redis.set(socket.id, user);
+    user = `${Number(user) + 1}`;
+
+    socket.on("disconnect", async () => {
+        const userId = await redis.get(socket.id);
+
+        if (userId) {
+            redis.del(userId);
+            redis.del(socket.id);
+        }
+    });
 })
 
 app.use(errorHandler);
