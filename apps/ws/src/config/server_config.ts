4a7bcee62e6e90848de7038bef359210a6495cd7
pankajtwts <pankajshadev@gmail.com>
2024-08-01 13:14:48 +0530

    fix: added redis uri

--- apps/ws/src/config/server_config.ts
+++ apps/ws/src/config/server_config.ts
@@ -5,4 +5,5 @@ dotenv.config();
 export const ServerConfig = {
     PORT: process.env.PORT || "3004",
     NODE_ENV: process.env.NODE_ENV,
+    REDIS_URI: process.env.REDIS_URI || "127.0.0.1:6379",
 }
