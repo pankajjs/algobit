6025a490e7630a7b3d4127aa79a4f009e6d604c6
pankajtwts <pankajshadev@gmail.com>
2024-08-02 21:19:41 +0530

    fix: added constants type

--- apps/executor/src/helper/constants.ts
+++ apps/executor/src/helper/constants.ts
@@ -1,17 +1,17 @@
 // contains info of stdin and stderr
-export const STREAM_HEADER_SIZE = 8
+export const STREAM_HEADER_SIZE: number = 8
 
-export const STDOUT_STREAM_TYPE = 1;
-export const STDERR_STREAM_TYPE = 2;
-export const DATA_LENGTH_BYTE_SIZE = 4;
+export const STDOUT_STREAM_TYPE: number = 1;
+export const STDERR_STREAM_TYPE: number = 2;
+export const DATA_LENGTH_BYTE_SIZE: number = 4;
 
-export const PYTHON_IMAGE = 'python:3.8-slim';
-export const JAVA_IMAGE = "openjdk:8-jdk-slim";
+export const PYTHON_IMAGE: string = 'python:3.8-slim';
+export const JAVA_IMAGE: string = "openjdk:8-jdk-slim";
 
-export const SUBMISSION_REQUEST_QUEUE = "SubmissionRequestQueue";
-export const SUBMISSION_REQUEST_JOB= "SubmissionRequestJob";
+export const SUBMISSION_REQUEST_QUEUE: string = "SubmissionRequestQueue";
+export const SUBMISSION_REQUEST_JOB: string = "SubmissionRequestJob";
 
-export const SUBMISSION_RESPONSE_QUEUE = "SubmissionResponseQueue";
-export const SUBMISSION_RESPONSE_JOB = "SubmissionResponseJob";
-export const PYTHON_TIME_LIMIT = 2;
-export const TIME_LIMIT_SECOND = 1000;
+export const SUBMISSION_RESPONSE_QUEUE: string = "SubmissionResponseQueue";
+export const SUBMISSION_RESPONSE_JOB: string = "SubmissionResponseJob";
+export const PYTHON_TIME_LIMIT: number = 2;
+export const TIME_LIMIT_SECOND: number = 1000;
