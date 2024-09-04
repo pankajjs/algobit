7840a376b95017209c600bb956714ad4bc53bc9e
pankajtwts <pankajshadev@gmail.com>
2024-09-04 15:15:13 +0530

    feat: add language-selector component

--- apps/client/components/editor.tsx
+++ apps/client/components/editor.tsx
@@ -1,35 +1,42 @@
 "use client"
 
 import CodeMirror from '@uiw/react-codemirror';
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { langs } from '@uiw/codemirror-extensions-langs';
 import { atomone } from '@uiw/codemirror-theme-atomone'
 import { historyField } from '@codemirror/commands';
 import { markdown, markdownLanguage } from '@codemirror/lang-markdown';
 import { languages } from '@codemirror/language-data'; 
+import { ChevronDown } from 'lucide-react';
+import { CheckIcon } from '@radix-ui/react-icons';
 
 const stateFields = { history: historyField };
 let extensions = [markdown({ base: markdownLanguage, codeLanguages: languages })]
+const languageList = ["java", "python"]
 
 export function AlgobitEditor({problem}:{problem: any}) {
   console.log(problem)
-  const [language, setLanguage] = useState("python");
+  const [language, setLanguage] = useState(languageList[0]);
   const languageCodeStub = problem.codestubs.find((stub: any, idx: number)=> stub.language === language);
   const [serializedState, setSerializedState] = useState<string|null>("");
   const [userSnippet, setUserSnippet] = useState(languageCodeStub.userSnippet);
+  const [showLanguageList, setShowLanguageList] = useState(false);
+  
+  console.log(extensions);
 
-
-  const loadChosenlanguage = useCallback((language: string) => {
-    const languages = {python:"python"}
-    if(language === languages.python){
-       return langs.python();
+  useEffect(()=>{
+    const loadChosenlanguage = (language:string) => {
+      const languages = {python:"python"}
+      if(language === languages.python){
+         return langs.python();
+      }
+  
+      return langs.java();
     }
 
-    return langs.java();
-  }, [])
+    extensions = [loadChosenlanguage(language), ...extensions]
 
-  extensions = [loadChosenlanguage(language), ...extensions]
-  console.log(extensions);
+  }, [language])
 
   useEffect(()=>{
     setUserSnippet(localStorage.getItem(language) || userSnippet.trim());
@@ -37,8 +44,27 @@ export function AlgobitEditor({problem}:{problem: any}) {
 
   },  [language, userSnippet])
 
+
   return (
     <div className='min-w-[50%]'>
+      <div className='py-1 border-b-2 border-[#383839] px-2 relative'>
+        <div
+        onClick={()=>setShowLanguageList(true)}
+        className='flex gap-1 capitalize px-3 py-1 items-center text-gray-200 text-sm border border-gray-700 hover:border-gray-400 hover:cursor-pointer rounded-md w-fit'>
+        {language} <ChevronDown size={15}/></div> 
+      </div>
+      {
+        showLanguageList && <div className='min-h-11 p-1 m-1 absolute min-w-20 bg-gray-800 z-50 text-slate-200 border border-gray-600 rounded-md'>
+          {
+            languageList.map((lang:string, idx:number)=>{
+              return <div onClick={()=>{
+                setLanguage(lang)
+                setShowLanguageList(false)
+              }} className='min-w-10 text-sm px-2 flex gap-1 items-center py-1 m-1 hover:cursor-pointer hover:border border-gray-600 rounded-md capitalize'>{lang=== language && <CheckIcon/>}{lang}</div>
+            })
+          }
+        </div>
+      }
     <CodeMirror
         theme={atomone}
         height='600px'
