10a69165ff26988dd078ced13a3005153a9106ee
pankajtwts <pankajshadev@gmail.com>
2024-09-04 15:15:20 +0530

    refactor

--- apps/client/components/testcase.tsx
+++ apps/client/components/testcase.tsx
@@ -9,11 +9,11 @@ export const Test = ({testCases}:{testCases:any})=>{
 
     return <div className="">
         <div className="border border-[#383839]">
-            <Button className="text-slate-400" variant={"link"} onClick={()=>setShowTestInput(true)}>Test Input</Button>
-            <Button className="text-slate-400" variant={"link"}>Test Result</Button>
+            <Button className="text-gray-300" variant={"link"} onClick={()=>setShowTestInput(true)}>Test Input</Button>
+            <Button className="text-slate-300" variant={"link"}>Test Result</Button>
         </div>
         {showTestInput && <div className="overflow-auto">
-            <pre className="text-slate-400 px-4">{testCases.map((testcase:any, _:number)=>testcase.input).join("\n")}</pre>
+            <pre className="text-gray-300 px-4">{testCases.map((testcase:any, _:number)=>testcase.input).join("\n")}</pre>
         </div>}
     </div>
 }
