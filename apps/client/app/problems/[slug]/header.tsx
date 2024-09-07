8515a1444278f90ebe3c6b62d1c86cdbd4e713fa
pankajtwts <pankajshadev@gmail.com>
2024-09-08 02:28:16 +0530

    fix: styling

--- apps/client/app/problems/[slug]/header.tsx
+++ apps/client/app/problems/[slug]/header.tsx
@@ -9,7 +9,7 @@ import { useSession } from "next-auth/react"
 export const Header = () => {
     const session: any = useSession();
 
-    return <div className="h-[7vh] px-5 flex items-center bg-gray-950">
+    return <div className="px-5 py-2 min-h-12 flex items-center bg-gray-950 min-w-full">
        <div className="flex w-full justify-center gap-5 text-xs">
             <Run/>
             <Submit/>
