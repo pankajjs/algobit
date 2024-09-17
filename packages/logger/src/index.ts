3a9438272fd1ed27660a74f37cb7f42cfce27a16
pankajtwts <pankajshadev@gmail.com>
2024-09-17 17:34:36 +0530

    feat: setup logger package

---
+++ packages/logger/src/index.ts
@@ -0,0 +1,23 @@
+import { createLogger, format, transports, transport } from "winston";
+
+const { combine, timestamp, prettyPrint, json, colorize } = format;
+const { Console } = transports;
+
+const loggerTransports: transport[]  = []
+
+loggerTransports.push(
+  new Console({
+    format:combine(
+      json(),
+      timestamp(),
+      prettyPrint(),
+      colorize({ all:true })
+    ),
+}));
+
+const logger = createLogger({
+  transports: loggerTransports,
+});
+
+export default logger;
+export type logger = typeof logger;
