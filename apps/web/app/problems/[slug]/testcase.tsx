22a3d7efed77ec6ce2c1efab15530abe6f6bdccc
pankajtwts <pankajshadev@gmail.com>
2024-09-21 00:36:44 +0530

    fix: test result

--- apps/web/app/problems/[slug]/testcase.tsx
+++ apps/web/app/problems/[slug]/testcase.tsx
@@ -4,12 +4,14 @@ import { useCallback, useContext, useEffect, useState } from "react"
 import { Button } from "../../../components/ui/button"
 import { socket } from "@/socket";
 import { RunResponseContext } from "./RunResponseContext";
+import { Label } from "@/components/ui/label";
 
 export const Test = ({testCases}:{testCases:any})=>{
     const [showTestInput, setShowTestInput] = useState(false);
     const [showTestResult, setShowTestResult] = useState(false);
-    const [testResult, setTestResult] = useState(null);
+    const [testResult, setTestResult] = useState<any>();
     const {setIsRunResponse} = useContext(RunResponseContext);
+    const [testCase, setTestCase] = useState(1);
 
 
     const handleRunResponse = useCallback((data: any) => {
@@ -28,14 +30,15 @@ export const Test = ({testCases}:{testCases:any})=>{
         };
     }, [handleRunResponse]);
 
+    console.log(testResult);
 
     return <div className="">
         <div className="border border-[#383839]">
-            <Button className="text-gray-300" variant={"link"} onClick={()=>{
+            <Button className={(showTestInput && "underline") + " text-gray-300"} variant={"link"} onClick={()=>{
                 setShowTestResult(false);
-                setShowTestInput(true)
+                setShowTestInput(true);
             }}>Test Input</Button>
-            <Button className="text-slate-300" variant={"link"} onClick={
+            <Button className={(showTestResult && 'underline') + " text-slate-300"} variant={"link"} onClick={
                 ()=>{
                     setShowTestInput(false);
                     setShowTestResult(true);
@@ -46,8 +49,49 @@ export const Test = ({testCases}:{testCases:any})=>{
             <pre className="text-gray-300 px-4">{testCases.map((testcase:any, _:number)=>testcase.input).join("\n")}</pre>
         </div>}
 
-        {showTestResult && <div>{
-            JSON.stringify(testResult)}
+        {showTestResult && testResult && <div>
+            
+            {testResult.status === "Error" && <div className="text-red-600">
+                <div className="border-b-2 border-[#383839] p-2">{testResult.status}</div>
+                <div className="p-2">{testResult.error}</div>
+                </div>
+            }
+
+            {testResult.status !== "Error" && 
+            <div className="flex flex-col">
+                <div className={(testResult.status === "WA" ? "text-red-700": "text-green-700") + " border-b-2 border-[#383839] p-2"}>{testResult.status ==="WA"?"Wrong Answer":testResult.status}</div>
+                <div className="flex gap-4 p-2">
+                    {
+                    testResult.input.map((_:string, idx:number)=>{
+                        return <Button key={idx} onClick={()=>{
+                            setTestCase(idx+ 1)
+                        }} variant={"secondary"}>Test #{idx+1}</Button>
+                    })
+                }
+                </div>
+                <div className="">
+                    {testResult.input.map((_:string, idx:number)=>{
+                    return <div key={idx} className="px-2">
+                        {testCase === idx + 1 && <div>
+                            <Label className="text-slate-50">Input</Label>
+                            <pre className={(testResult.status === "WA" ? "text-red-700": "text-green-700") + " border-gray-700 border rounded-md px-4 py-2"}>
+                                    {testResult.input[idx]}
+                            </pre>
+                            <Label className="text-slate-50">Output</Label>
+                            <pre className={(testResult.status === "WA" ? "text-red-700": "text-green-700") + " border-gray-700 border rounded-md px-4 py-2"}>
+                                    {testResult.output[idx]}
+                            </pre>
+                            <Label className="text-slate-50">Expected Output</Label>
+                            <pre className={(testResult.status === "WA" ? "text-red-700": "text-green-700") + " border-gray-700 border rounded-md px-4 py-2"}>
+                                    {testResult.expectedOutput[idx]}
+                            </pre>
+                        </div>}
+                    </div>
+                })}
+                </div>
+                
+            </div>
+            }
         </div>}
     </div>
 }
