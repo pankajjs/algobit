cc1b12f0964954ddb41b7da324746250cad94f6f
pankajtwts <pankajshadev@gmail.com>
2024-09-20 22:43:29 +0530

    Refactor: spinner

--- apps/web/app/problems/[slug]/submit-button.tsx
+++ apps/web/app/problems/[slug]/submit-button.tsx
@@ -8,6 +8,7 @@ import { socket } from "@/socket";
 import { useSession } from "next-auth/react";
 import axios from "axios";
 import { SubmissionResponseContext } from "./SubmissionResponseContext";
+import { LoadingSpinner } from "@/components/spinner";
 
 const Submission_Service_Api = "http://localhost:5003"
 
@@ -42,33 +43,3 @@ export const Submit = ()=>{
            <UploadIcon size={15}/> Submit
         </Button>
 }
-
-export interface ISVGProps extends React.SVGProps<SVGSVGElement> {
-    size?: number;
-    className?: string;
-  }
-  
-
-export const LoadingSpinner = ({
-    size = 18,
-    className,
-    ...props
-  }: ISVGProps) => {
-    return (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width={size}
-        height={size}
-        {...props}
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        className={"animate-spin " +  className}
-      >
-        <path d="M21 12a9 9 0 1 1-6.219-8.56" />
-      </svg>
-    );
-  };
